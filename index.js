fe904f87a2ec73ef83774cb7949df4b0944690cd
zarcode <denciczarko@gmail.com>
2023-07-07 12:16:35 +0200

    make index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+export { defaultData } from "./default.json";
+export { financialData } from "./financial.json";
+export { fitnessData } from "./fitness.json";
+export { gamingData } from "./gaming.json";
