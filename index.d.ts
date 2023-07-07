449bc5206b85dadd024105e7c9f4e951b97a6d9d
zarcode <denciczarko@gmail.com>
2023-07-07 12:37:08 +0200

    ts declarations

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare const defaultData: any;
+declare const financialData: any;
+declare const fitnessData: any;
+declare const gamingData: any;
+
+export {
+  defaultData,
+  financialData,
+  fitnessData,
+  gamingData,
+};
