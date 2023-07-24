31c1960642801d0e3e916da0957bf68d9ab019eb
zarcode <denciczarko@gmail.com>
2023-07-24 09:58:45 +0200

    change exports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import defaultConfig from "./default.json";
-import financialConfig from "./financial.json";
-import fitnessConfig from "./fitness.json";
-import gamingConfig from "./gaming.json";
+import defaultConfig from "./transformed/default.json";
+import financialConfig from "./transformed/financial.json";
+import fitnessConfig from "./transformed/fitness.json";
+import gamingConfig from "./transformed/gaming.json";
 
 export { defaultConfig, financialConfig, fitnessConfig, gamingConfig };
