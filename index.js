22cd76285dee70775998057d6c1a70a08ae6973a
zarcode <denciczarko@gmail.com>
2023-08-03 11:23:01 +0200

    add stylepropsMap

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import financialConfig from "./transformed/financial.json";
 import fitnessConfig from "./transformed/fitness.json";
 import gamingConfig from "./transformed/gaming.json";
 import referencesMap from "./references-map.json";
+import stylepropsMap from "./styleprops-map.json";
 
 export {
   defaultConfig,
@@ -10,4 +11,5 @@ export {
   fitnessConfig,
   gamingConfig,
   referencesMap,
+  stylepropsMap,
 };
