7db891c7a3657fd94329518779692bc990b58867
zarcode <denciczarko@gmail.com>
2023-08-24 17:43:57 +0200

    move json map files

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import defaultConfig from "./transformed/default.json";
 import financialConfig from "./transformed/financial.json";
 import fitnessConfig from "./transformed/fitness.json";
 import gamingConfig from "./transformed/gaming.json";
-import referencesMap from "./references-map.json";
-import stylepropsMap from "./styleprops-map.json";
+import referencesMap from "./scripts/references-map.json";
+import stylepropsMap from "./scripts/styleprops-map.json";
 
 export {
   defaultConfig,
